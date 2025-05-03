ce219dd88ac9588bf32037330312f40cedf9e26e
quy <quilvph47068@fpt.edu.vn>
2025-05-03 09:41:23 +0700

    Update

--- DATN/FE/src/pages/website/components/Footer.jsx
+++ DATN/FE/src/pages/website/components/Footer.jsx
@@ -4,8 +4,11 @@ import {
   EnvironmentOutlined,
   PhoneOutlined,
   MailOutlined,
+  SyncOutlined,
+  CarOutlined,
 } from "@ant-design/icons";
-
+import logo from "../../image/logo.png";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div style={{ backgroundColor: "#000", color: "#fff", padding: "30px 0" }}>
@@ -19,37 +22,55 @@ const Footer = () => {
             alignItems: "center",
           }}
         >
-          <img
-            src=""
-            alt="Ibee"
-            style={{ width: "100px", height: "100px", objectFit: "contain" }}
-          />
-          <h3 style={{ marginTop: "10px" }}>Ibee</h3>
+          <div className="logo">
+            <Link to={"/"}>
+              <img
+                src={logo}
+                alt="Ibee"
+                style={{
+                  objectFit: "contain",
+                  height: "200px",
+                  width: "300px",
+                }}
+              />
+            </Link>
+          </div>
         </Col>
         <Col xs={24} md={8}>
           <h3 style={{ marginBottom: "15px", fontWeight: "bold" }}>
             CHÍNH SÁCH
           </h3>
-          <ul style={{ listStyle: "none", paddingLeft: 0 }}>
-            <li>✅ Chính sách bảo hành</li>
-            <li>🔄 Chính sách đổi trả</li>
-            <li>📑 Chính sách xuất hóa đơn</li>
-            <li>⚪ Chính sách chứng nhận an toàn thực phẩm</li>
-          </ul>
+          <div style={{ fontSize: "16px", lineHeight: "1.6" }}>
+            <p>
+              <SyncOutlined style={{ color: "#1890ff", marginRight: 8 }} />
+              Hư gì đổi nấy 12 tháng tại cửa hàng ,chính sách bảo hành, đổi trả
+            </p>
+            <p>
+              <CarOutlined style={{ color: "#faad14", marginRight: 8 }} />
+              Giao hàng nhanh toàn quốc
+            </p>
+          </div>
         </Col>
         <Col xs={24} md={8}>
           <h3 style={{ marginBottom: "15px", fontWeight: "bold" }}>
             LIÊN VỚI CHÚNG TÔI
           </h3>
-          <p>
-            <EnvironmentOutlined /> Công ty Công Nghệ Ibee
-          </p>
-          <p>
-            <PhoneOutlined /> 0763.272.301
-          </p>
-          <p>
-            <MailOutlined /> ibee@gmail.com
-          </p>
+          <div style={{ fontSize: "16px", lineHeight: "1.6" }}>
+            <p>
+              <EnvironmentOutlined
+                style={{ color: "#FFA500", marginRight: 8 }}
+              />
+              Công ty Công Nghệ Ibee
+            </p>
+            <p>
+              <PhoneOutlined style={{ color: "#f5222d", marginRight: 8 }} />
+              0763.272.301
+            </p>
+            <p>
+              <MailOutlined style={{ color: "#1E90FF", marginRight: 8 }} />
+              ibee@gmail.com
+            </p>
+          </div>
         </Col>
       </Row>
     </div>
@@ -57,4 +78,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
